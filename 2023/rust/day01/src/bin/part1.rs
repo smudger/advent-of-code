8e72ae0aa701964652678702e453fa1efb7b3565
Louis Smith <15144861+smudger@users.noreply.github.com>
2023-12-01 15:36:29 +0000

    Solve day 1

--- 2023/rust/day01/src/bin/part1.rs
+++ 2023/rust/day01/src/bin/part1.rs
@@ -5,7 +5,19 @@ fn main() {
 }
 
 fn solve(input: &str) -> String {
-    "Hello, world!".to_string()
+    input
+        .lines()
+        .map(|line| {
+            line
+                .chars()
+                .filter_map(|character| {
+                    character.to_digit(10)
+                })
+                .collect::<Vec<u32>>()
+        })
+        .map(|digits| (digits.first().unwrap() * 10) + digits.last().unwrap())
+        .sum::<u32>()
+        .to_string()
 }
 
 #[cfg(test)]
@@ -13,8 +25,17 @@ mod tests {
     use super::*;
 
     #[test]
+    fn it_solves_the_example() {
+        let input = "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet";
+        assert_eq!(solve(input), "142");
+    }
+    
+    #[test]
     fn it_solves_the_puzzle() {
-        let input = include_str!("./example.txt");
-        assert_eq!(solve(input), "Hello, world!");
+        let input = include_str!("./input.txt");
+        assert_eq!(solve(input), "53334");
     }
 }
--- 2023/rust/day01/src/bin/part2.rs
+++ 2023/rust/day01/src/bin/part2.rs
@@ -1,11 +1,63 @@
+use std::cmp::{max, min};
+
 fn main() {
     let input = include_str!("./input.txt");
     let output = solve(input);
     dbg!(output);
 }
 
+const NUMBERS: [(&str, &str); 10] = [
+    ("0", "zero"),
+    ("1", "one"),
+    ("2", "two"),
+    ("3", "three"),
+    ("4", "four"),
+    ("5", "five"),
+    ("6", "six"),
+    ("7", "seven"),
+    ("8", "eight"),
+    ("9", "nine"),
+];
+
 fn solve(input: &str) -> String {
-    "Hello, world!".to_string()
+    input
+        .lines()
+        .map(|line| {
+            let mut first_indexes = NUMBERS
+                .iter()
+                .enumerate()
+                .map(|(index, (digit, text))| {
+                    match (line.find(digit), line.find(text)) {
+                        (Some(i), Some(j)) => (index, Some(min(i, j))),
+                        (Some(i), None) => (index, Some(i)),
+                        (None, Some(j)) => (index, Some(j)),
+                        (None, None) => (index, None),
+                    }
+                })
+                .filter(|(_, location)| location.is_some())
+                .collect::<Vec<(usize, Option<usize>)>>();
+            first_indexes.sort_by(|a, b| a.1.cmp(&b.1));
+            
+            let mut last_indexes = NUMBERS
+                .iter()
+                .enumerate()
+                .map(|(index, (digit, text))| {
+                    match (line.rfind(digit), line.rfind(text)) {
+                        (Some(i), Some(j)) => (index, Some(max(i, j))),
+                        (Some(i), None) => (index, Some(i)),
+                        (None, Some(j)) => (index, Some(j)),
+                        (None, None) => (index, None),
+                    }
+                })
+                .filter(|(_, location)| location.is_some())
+                .collect::<Vec<(usize, Option<usize>)>>();
+            last_indexes.sort_by(|a, b| a.1.cmp(&b.1));
+
+            (first_indexes.first().unwrap().0, last_indexes.last().unwrap().0)
+        })
+        .map(|(a, b)| (a * 10) + b)
+        .sum::<usize>()
+        .to_string()
 }
 
 #[cfg(test)]
@@ -13,8 +65,21 @@ mod tests {
     use super::*;
 
     #[test]
+    fn it_solves_the_example() {
+        let input = "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+9seveneight9
+7pqrstsixteen";
+        assert_eq!(solve(input), "380");
+    }
+
+    #[test]
     fn it_solves_the_puzzle() {
-        let input = include_str!("./example.txt");
-        assert_eq!(solve(input), "Hello, world!");
+        let input = include_str!("./input.txt");
+        assert_eq!(solve(input), "52834");
     }
 }
