5e70c3385c912a9832efd848ed6b6fd047f0d221
Louis Smith <15144861+smudger@users.noreply.github.com>
2023-12-07 15:39:57 +0000

    Solve day07 part 2

--- 2023/rust/day07/src/bin/part2.rs
+++ 2023/rust/day07/src/bin/part2.rs
@@ -1,11 +1,101 @@
+use itertools::Itertools;
+
 fn main() {
     let input = include_str!("./input.txt");
     let output = solve(input);
     dbg!(output);
 }
 
+const HANDS: [[usize; 5]; 7] = [
+    [1, 1, 1, 1, 1],
+    [2, 1, 1, 1, 0],
+    [2, 2, 1, 0, 0],
+    [3, 1, 1, 0, 0],
+    [3, 2, 0, 0, 0],
+    [4, 1, 0, 0, 0],
+    [5, 0, 0, 0, 0],
+];
+
+const CARDS: [char; 13] = ['J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A'];
+
 fn solve(input: &str) -> String {
-    todo!("part 2");
+    let mut hands = input
+        .lines()
+        .map(|line| {
+            let (hand, bid) = line
+                .split_once(" ")
+                .expect("the line contains a space");
+
+            (
+                hand,
+                bid.parse::<usize>().expect("the bid is a u32")
+            )
+        })
+        .collect::<Vec<_>>();
+
+    hands.sort_unstable_by(|a, b| {
+        let mut a_counts = a.0
+            .chars()
+            .filter(|c| c != &'J')
+            .counts()
+            .into_values()
+            .sorted()
+            .rev()
+            .collect::<Vec<_>>();
+        if let None = a_counts.get(0) {
+            a_counts.push(0);
+        };
+        a_counts[0] += a.0.chars().filter(|c| c == &'J').count();
+        let mut b_counts = b.0
+            .chars()
+            .filter(|c| c != &'J')
+            .counts()
+            .into_values()
+            .sorted()
+            .rev()
+            .collect::<Vec<_>>();
+        if let None = b_counts.get(0) {
+            b_counts.push(0);
+        };
+        b_counts[0] += b.0.chars().filter(|c| c == &'J').count();
+
+        match a_counts == b_counts {
+            true => {
+                let (a_card, b_card) = a.0
+                    .chars()
+                    .zip(b.0.chars())
+                    .find(|(a, b)| a != b)
+                    .expect("a and b are not the same string");
+
+                let a_strength = CARDS.into_iter().find_position(|card| *card == a_card).expect("a_card is a valid card").0;
+                let b_strength = CARDS.into_iter().find_position(|card| *card == b_card).expect("b_card is a valid card").0;
+                a_strength.cmp(&b_strength)
+            },
+            false => {
+                let a_hand = HANDS
+                    .into_iter()
+                    .find_position(|counts| counts[..a_counts.len()] == a_counts[..])
+                    .expect("a_counts is a valid hand")
+                    .0;
+                let b_hand = HANDS
+                    .into_iter()
+                    .find_position(|counts| counts[..b_counts.len()] == b_counts[..])
+                    .expect("b_counts is a valid hand")
+                    .0;
+
+                a_hand.cmp(&b_hand)
+            }
+        }
+    });
+
+    hands
+        .iter()
+        .enumerate()
+        .map(|(index, (_, bid))| {
+            (index + 1) * bid
+        })
+        .sum::<usize>()
+        .to_string()
 }
 
 #[cfg(test)]
@@ -14,13 +104,17 @@ mod tests {
 
     #[test]
     fn it_solves_the_example() {
-        let input = "";
-        assert_eq!(solve(input), "Hello, world!");
+        let input = "32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483";
+        assert_eq!(solve(input), "5905");
     }
 
-    // #[test]
-    // fn it_solves_the_puzzle() {
-    //     let input = include_str!("./input.txt");
-    //     assert_eq!(solve(input), "Hello, world!");
-    // }
+    #[test]
+    fn it_solves_the_puzzle() {
+        let input = include_str!("./input.txt");
+        assert_eq!(solve(input), "251824095");
+    }
 }
