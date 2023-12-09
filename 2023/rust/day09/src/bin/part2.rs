3086795503c9792cf1be6877b85b5208c805bb33
Louis Smith <15144861+smudger@users.noreply.github.com>
2023-12-09 16:33:16 +0000

    Solve day09 part2

--- 2023/rust/day09/src/bin/part2.rs
+++ 2023/rust/day09/src/bin/part2.rs
@@ -1,3 +1,7 @@
+use std::str::FromStr;
+use itertools::FoldWhile::{Continue, Done};
+use itertools::Itertools;
+
 fn main() {
     let input = include_str!("./input.txt");
     let output = solve(input);
@@ -5,7 +9,43 @@ fn main() {
 }
 
 fn solve(input: &str) -> String {
-    todo!("part 2");
+    input
+        .lines()
+        .map(|line| {
+            let sequence = line
+                .split_whitespace()
+                .map(i32::from_str)
+                .flatten()
+                .collect::<Vec<_>>();
+
+            let sequences = (0..)
+                .into_iter()
+                .fold_while(vec![sequence], |mut acc, _| {
+                    let diffs = acc
+                        .last()
+                        .expect("there is a sequence to consider")
+                        .windows(2)
+                        .map(|w| w[1] - w[0])
+                        .collect::<Vec<_>>();
+
+                    acc.push(diffs.clone());
+
+                    match diffs.windows(2).all(|w| w[0] == w[1]) {
+                        true => Done(acc),
+                        false => Continue(acc)
+                    }
+                })
+                .into_inner();
+
+            sequences
+                .iter()
+                .rev()
+                .fold(0i32, |acc, seq| {
+                    seq.first().expect("each sequence contains at least one element") - acc
+                })
+        })
+        .sum::<i32>()
+        .to_string()
 }
 
 #[cfg(test)]
@@ -14,13 +54,15 @@ mod tests {
 
     #[test]
     fn it_solves_the_example() {
-        let input = "";
-        assert_eq!(solve(input), "Hello, world!");
+        let input = "0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45";
+        assert_eq!(solve(input), "2");
     }
 
-    // #[test]
-    // fn it_solves_the_puzzle() {
-    //     let input = include_str!("./input.txt");
-    //     assert_eq!(solve(input), "Hello, world!");
-    // }
+    #[test]
+    fn it_solves_the_puzzle() {
+        let input = include_str!("./input.txt");
+        assert_eq!(solve(input), "919");
+    }
 }
