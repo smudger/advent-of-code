b307667f1667cf60e31f59ae96cded5f104dafc0
Louis Smith <15144861+smudger@users.noreply.github.com>
2023-12-17 10:00:32 +0000

    Move day16 part1 to queue

--- 2023/rust/day16/src/bin/part1.rs
+++ 2023/rust/day16/src/bin/part1.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, VecDeque};
 use glam::IVec2;
 use crate::Direction::{Down, Left, Right, Up};
 use crate::Tile::{BackwardMirror, Empty, ForwardMirror, HorizontalSplitter, VerticalSplitter};
@@ -20,10 +20,10 @@ enum Direction {
 impl Direction {
     fn move_vec(&self) -> IVec2 {
         match self {
-            Direction::Up => IVec2::NEG_Y,
-            Direction::Down => IVec2::Y,
-            Direction::Left => IVec2::NEG_X,
-            Direction::Right => IVec2::X,
+            Up => IVec2::NEG_Y,
+            Down => IVec2::Y,
+            Left => IVec2::NEG_X,
+            Right => IVec2::X,
         }
     }
 }
@@ -58,144 +58,129 @@ fn solve(input: &str) -> String {
         .collect::<Vec<_>>();
     let starting_position = Position(IVec2 { x: 0, y: 0 }, Right);
     
-    let (_, energized_tiles) = calculate_energized_tiles(starting_position, &grid, HashSet::new());
+    let energized_tiles = calculate_energized_tiles(starting_position, &grid);
     energized_tiles.len().to_string()
 }
 
-fn calculate_energized_tiles(current_pos: Position, grid: &Vec<Vec<Tile>>, mut visited_positions: HashSet<Position>) -> (HashSet<Position>, HashSet<IVec2>) {
+fn calculate_energized_tiles(starting_pos: Position, grid: &Vec<Vec<Tile>>) -> HashSet<IVec2> {
+    let mut queue = VecDeque::new();
+    queue.push_back(starting_pos);
+    
     let mut energized_tiles = HashSet::new();
-    if visited_positions.contains(&current_pos) {
-        return (visited_positions, energized_tiles);
-    };
+    let mut visited_positions = HashSet::new();
     
-    visited_positions.insert(current_pos);
-    energized_tiles.insert(current_pos.0);
-    
-    let current_tile = grid
-        .get(current_pos.0.y as usize) 
-        .expect("the current position y is in the grid")
-        .get(current_pos.0.x as usize)
-        .expect("the current position x is in the grid");
-    
-    match current_tile {
-        Empty => {
-            let new_loc = current_pos.0 + current_pos.1.move_vec();
-            
-            let Some(row) = grid.get(new_loc.y as usize) else {
-                return (visited_positions, energized_tiles);
-            };
-            
-            let Some(_) = row.get(new_loc.x as usize) else {
-                return (visited_positions, energized_tiles);
-            };
-            
-            let (new_visited_positions, new_energized_tiles) = calculate_energized_tiles(Position(new_loc, current_pos.1), grid, visited_positions);
-            (new_visited_positions, energized_tiles
-                .union(&new_energized_tiles)
-                .map(|vec| *vec)
-                .collect())
-        }
-        VerticalSplitter => {
-            let new_dirs = match current_pos.1 {
-                Up => vec![Up],
-                Down => vec![Down],
-                Left | Right => vec![Up, Down],
-            };
+    while let Some(current_pos) = queue.pop_front() {
+        if visited_positions.contains(&current_pos) {
+            continue;
+        };
 
-            return new_dirs
-                .into_iter()
-                .fold((visited_positions, energized_tiles), |acc, new_dir| {
+        visited_positions.insert(current_pos);
+        energized_tiles.insert(current_pos.0);
+        
+        let current_tile = grid
+            .get(current_pos.0.y as usize)
+            .expect("the current position y is in the grid")
+            .get(current_pos.0.x as usize)
+            .expect("the current position x is in the grid");
+
+        match current_tile {
+            Empty => {
+                let new_loc = current_pos.0 + current_pos.1.move_vec();
+
+                let Some(row) = grid.get(new_loc.y as usize) else {
+                    continue;
+                };
+
+                let Some(_) = row.get(new_loc.x as usize) else {
+                    continue;
+                };
+
+                queue.push_back(Position(new_loc, current_pos.1));
+            }
+            VerticalSplitter => {
+                let new_dirs = match current_pos.1 {
+                    Up => vec![Up],
+                    Down => vec![Down],
+                    Left | Right => vec![Up, Down],
+                };
+
+                for new_dir in new_dirs.into_iter() {
                     let new_loc = current_pos.0 + new_dir.move_vec();
 
                     let Some(row) = grid.get(new_loc.y as usize) else {
-                        return acc;
+                        continue;
                     };
 
                     let Some(_) = row.get(new_loc.x as usize) else {
-                        return acc;
+                        continue;
                     };
 
-                    let (new_visited_positions, new_energized_tiles) = calculate_energized_tiles(Position(new_loc, new_dir), grid, acc.0);
-                    (new_visited_positions, acc.1
-                        .union(&new_energized_tiles)
-                        .map(|vec| *vec)
-                        .collect())
-                });
-        },
-        HorizontalSplitter => {
-            let new_dirs = match current_pos.1 {
-                Left => vec![Left],
-                Right => vec![Right],
-                Up | Down => vec![Left, Right],
-            };
+                    queue.push_back(Position(new_loc, new_dir));
+                }
+            },
+            HorizontalSplitter => {
+                let new_dirs = match current_pos.1 {
+                    Left => vec![Left],
+                    Right => vec![Right],
+                    Up | Down => vec![Left, Right],
+                };
 
-            return new_dirs
-                .into_iter()
-                .fold((visited_positions, energized_tiles), |acc, new_dir| {
+                for new_dir in new_dirs.into_iter() {
                     let new_loc = current_pos.0 + new_dir.move_vec();
 
                     let Some(row) = grid.get(new_loc.y as usize) else {
-                        return acc;
+                        continue;
                     };
 
                     let Some(_) = row.get(new_loc.x as usize) else {
-                        return acc;
+                        continue;
                     };
 
-                    let (new_visited_positions, new_energized_tiles) = calculate_energized_tiles(Position(new_loc, new_dir), grid, acc.0);
-                    (new_visited_positions, acc.1
-                        .union(&new_energized_tiles)
-                        .map(|vec| *vec)
-                        .collect())
-                });
-        }
-        ForwardMirror => {
-            let new_dir = match current_pos.1 {
-                Up => Right,
-                Down => Left,
-                Left => Down,
-                Right => Up,
-            };
-            let new_loc = current_pos.0 + new_dir.move_vec();
+                    queue.push_back(Position(new_loc, new_dir));
+                }
+            }
+            ForwardMirror => {
+                let new_dir = match current_pos.1 {
+                    Up => Right,
+                    Down => Left,
+                    Left => Down,
+                    Right => Up,
+                };
+                let new_loc = current_pos.0 + new_dir.move_vec();
 
-            let Some(row) = grid.get(new_loc.y as usize) else {
-                return (visited_positions, energized_tiles);
-            };
+                let Some(row) = grid.get(new_loc.y as usize) else {
+                    continue;
+                };
 
-            let Some(_) = row.get(new_loc.x as usize) else {
-                return (visited_positions, energized_tiles);
-            };
+                let Some(_) = row.get(new_loc.x as usize) else {
+                    continue;
+                };
 
-            let (new_visited_positions, new_energized_tiles) = calculate_energized_tiles(Position(new_loc, new_dir), grid, visited_positions);
-            (new_visited_positions, energized_tiles
-                .union(&new_energized_tiles)
-                .map(|vec| *vec)
-                .collect())
-        }
-        BackwardMirror => {
-            let new_dir = match current_pos.1 {
-                Up => Left,
-                Down => Right,
-                Left => Up,
-                Right => Down,
-            };
-            let new_loc = current_pos.0 + new_dir.move_vec();
+                queue.push_back(Position(new_loc, new_dir));
+            }
+            BackwardMirror => {
+                let new_dir = match current_pos.1 {
+                    Up => Left,
+                    Down => Right,
+                    Left => Up,
+                    Right => Down,
+                };
+                let new_loc = current_pos.0 + new_dir.move_vec();
 
-            let Some(row) = grid.get(new_loc.y as usize) else {
-                return (visited_positions, energized_tiles);
-            };
+                let Some(row) = grid.get(new_loc.y as usize) else {
+                    continue;
+                };
 
-            let Some(_) = row.get(new_loc.x as usize) else {
-                return (visited_positions, energized_tiles);
-            };
+                let Some(_) = row.get(new_loc.x as usize) else {
+                    continue;
+                };
 
-            let (new_visited_positions, new_energized_tiles) = calculate_energized_tiles(Position(new_loc, new_dir), grid, visited_positions);
-            (new_visited_positions, energized_tiles
-                .union(&new_energized_tiles)
-                .map(|vec| *vec)
-                .collect())
+                queue.push_back(Position(new_loc, new_dir));
+            }
         }
     }
+    
+    energized_tiles
 }
 
 #[cfg(test)]
@@ -218,9 +203,9 @@ mod tests {
         assert_eq!(solve(input), "46");
     }
 
-    // #[test]
-    // fn it_solves_the_puzzle() {
-    //     let input = include_str!("./input.txt");
-    //     assert_eq!(solve(input), "7392");
-    // }
+    #[test]
+    fn it_solves_the_puzzle() {
+        let input = include_str!("./input.txt");
+        assert_eq!(solve(input), "7392");
+    }
 }
--- 2023/rust/day16/src/bin/part2.rs
+++ 2023/rust/day16/src/bin/part2.rs
@@ -1,11 +1,256 @@
+use std::collections::HashSet;
+use glam::IVec2;
+use indicatif::ParallelProgressIterator;
+use rayon::prelude::IntoParallelIterator;
+use crate::Direction::{Down, Left, Right, Up};
+use crate::Tile::{BackwardMirror, Empty, ForwardMirror, HorizontalSplitter, VerticalSplitter};
+use rayon::iter::ParallelIterator;
+
 fn main() {
     let input = include_str!("./input.txt");
     let output = solve(input);
     dbg!(output);
 }
 
+#[derive(Debug, Copy, Clone, PartialEq, Hash, Eq)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl Direction {
+    fn move_vec(&self) -> IVec2 {
+        match self {
+            Direction::Up => IVec2::NEG_Y,
+            Direction::Down => IVec2::Y,
+            Direction::Left => IVec2::NEG_X,
+            Direction::Right => IVec2::X,
+        }
+    }
+}
+
+#[derive(Debug)]
+enum Tile {
+    Empty,
+    ForwardMirror,
+    BackwardMirror,
+    VerticalSplitter,
+    HorizontalSplitter,
+}
+
+#[derive(Debug, Copy, Clone, Eq, Hash, PartialEq)]
+struct Position(IVec2, Direction);
+
 fn solve(input: &str) -> String {
-    todo!("part 2");
+    let grid = input
+        .lines()
+        .map(|line| line
+            .chars()
+            .map(|c| match c {
+                '.' => Empty,
+                '/' => ForwardMirror,
+                '\\' => BackwardMirror,
+                '-' => HorizontalSplitter,
+                '|' => VerticalSplitter,
+                _ => unimplemented!("unknown character: {}", c)
+            })
+            .collect::<Vec<_>>()
+        )
+        .collect::<Vec<_>>();
+
+    let row_len = grid[0].len();
+    
+    let max_left_start = (0..grid.len())
+        .into_par_iter()
+        .map(|y| {
+            let starting_position = Position(IVec2 { x: 0, y: y as i32 }, Right);
+            
+            let (_, energized_tiles) = calculate_energized_tiles(starting_position, &grid, HashSet::new());
+            
+            energized_tiles.len()
+        })
+        .max()
+        .unwrap()
+        .to_string();
+    let max_right_start = (0..grid.len())
+        .into_par_iter()
+        .map(|y| {
+            let starting_position = Position(IVec2 { x: (row_len - 1) as i32, y: y as i32 }, Left);
+
+            let (_, energized_tiles) = calculate_energized_tiles(starting_position, &grid, HashSet::new());
+
+            energized_tiles.len()
+        })
+        .max()
+        .unwrap()
+        .to_string();
+    let max_top_start = (0..row_len)
+        .into_par_iter()
+        .map(|x| {
+            let starting_position = Position(IVec2 { x: x as i32, y: 0 }, Down);
+
+            let (_, energized_tiles) = calculate_energized_tiles(starting_position, &grid, HashSet::new());
+
+            energized_tiles.len()
+        })
+        .max()
+        .unwrap()
+        .to_string();
+    let max_bottom_start = (0..row_len)
+        .into_par_iter()
+        .map(|x| {
+            let starting_position = Position(IVec2 { x: x as i32, y: (grid.len() - 1) as i32 }, Up);
+
+            let (_, energized_tiles) = calculate_energized_tiles(starting_position, &grid, HashSet::new());
+
+            energized_tiles.len()
+        })
+        .max()
+        .unwrap()
+        .to_string();
+    
+    max_left_start
+        .max(max_bottom_start)
+        .max(max_right_start)
+        .max(max_top_start)
+}
+
+fn calculate_energized_tiles(current_pos: Position, grid: &Vec<Vec<Tile>>, mut visited_positions: HashSet<Position>) -> (HashSet<Position>, HashSet<IVec2>) {
+    let mut energized_tiles = HashSet::new();
+    if visited_positions.contains(&current_pos) {
+        return (visited_positions, energized_tiles);
+    };
+
+    visited_positions.insert(current_pos);
+    energized_tiles.insert(current_pos.0);
+
+    let current_tile = grid
+        .get(current_pos.0.y as usize)
+        .expect("the current position y is in the grid")
+        .get(current_pos.0.x as usize)
+        .expect("the current position x is in the grid");
+
+    match current_tile {
+        Empty => {
+            let new_loc = current_pos.0 + current_pos.1.move_vec();
+
+            let Some(row) = grid.get(new_loc.y as usize) else {
+                return (visited_positions, energized_tiles);
+            };
+
+            let Some(_) = row.get(new_loc.x as usize) else {
+                return (visited_positions, energized_tiles);
+            };
+
+            let (new_visited_positions, new_energized_tiles) = calculate_energized_tiles(Position(new_loc, current_pos.1), grid, visited_positions);
+            (new_visited_positions, energized_tiles
+                .union(&new_energized_tiles)
+                .map(|vec| *vec)
+                .collect())
+        }
+        VerticalSplitter => {
+            let new_dirs = match current_pos.1 {
+                Up => vec![Up],
+                Down => vec![Down],
+                Left | Right => vec![Up, Down],
+            };
+
+            return new_dirs
+                .into_iter()
+                .fold((visited_positions, energized_tiles), |acc, new_dir| {
+                    let new_loc = current_pos.0 + new_dir.move_vec();
+
+                    let Some(row) = grid.get(new_loc.y as usize) else {
+                        return acc;
+                    };
+
+                    let Some(_) = row.get(new_loc.x as usize) else {
+                        return acc;
+                    };
+
+                    let (new_visited_positions, new_energized_tiles) = calculate_energized_tiles(Position(new_loc, new_dir), grid, acc.0);
+                    (new_visited_positions, acc.1
+                        .union(&new_energized_tiles)
+                        .map(|vec| *vec)
+                        .collect())
+                });
+        },
+        HorizontalSplitter => {
+            let new_dirs = match current_pos.1 {
+                Left => vec![Left],
+                Right => vec![Right],
+                Up | Down => vec![Left, Right],
+            };
+
+            return new_dirs
+                .into_iter()
+                .fold((visited_positions, energized_tiles), |acc, new_dir| {
+                    let new_loc = current_pos.0 + new_dir.move_vec();
+
+                    let Some(row) = grid.get(new_loc.y as usize) else {
+                        return acc;
+                    };
+
+                    let Some(_) = row.get(new_loc.x as usize) else {
+                        return acc;
+                    };
+
+                    let (new_visited_positions, new_energized_tiles) = calculate_energized_tiles(Position(new_loc, new_dir), grid, acc.0);
+                    (new_visited_positions, acc.1
+                        .union(&new_energized_tiles)
+                        .map(|vec| *vec)
+                        .collect())
+                });
+        }
+        ForwardMirror => {
+            let new_dir = match current_pos.1 {
+                Up => Right,
+                Down => Left,
+                Left => Down,
+                Right => Up,
+            };
+            let new_loc = current_pos.0 + new_dir.move_vec();
+
+            let Some(row) = grid.get(new_loc.y as usize) else {
+                return (visited_positions, energized_tiles);
+            };
+
+            let Some(_) = row.get(new_loc.x as usize) else {
+                return (visited_positions, energized_tiles);
+            };
+
+            let (new_visited_positions, new_energized_tiles) = calculate_energized_tiles(Position(new_loc, new_dir), grid, visited_positions);
+            (new_visited_positions, energized_tiles
+                .union(&new_energized_tiles)
+                .map(|vec| *vec)
+                .collect())
+        }
+        BackwardMirror => {
+            let new_dir = match current_pos.1 {
+                Up => Left,
+                Down => Right,
+                Left => Up,
+                Right => Down,
+            };
+            let new_loc = current_pos.0 + new_dir.move_vec();
+
+            let Some(row) = grid.get(new_loc.y as usize) else {
+                return (visited_positions, energized_tiles);
+            };
+
+            let Some(_) = row.get(new_loc.x as usize) else {
+                return (visited_positions, energized_tiles);
+            };
+
+            let (new_visited_positions, new_energized_tiles) = calculate_energized_tiles(Position(new_loc, new_dir), grid, visited_positions);
+            (new_visited_positions, energized_tiles
+                .union(&new_energized_tiles)
+                .map(|vec| *vec)
+                .collect())
+        }
+    }
 }
 
 #[cfg(test)]
@@ -14,13 +259,23 @@ mod tests {
 
     #[test]
     fn it_solves_the_example() {
-        let input = "";
-        assert_eq!(solve(input), "Hello, world!");
+        let input = r".|...\....
+|.-.\.....
+.....|-...
+........|.
+..........
+.........\
+..../.\\..
+.-.-/..|..
+.|....-|.\
+..//.|....
+";
+        assert_eq!(solve(input), "51");
     }
 
     // #[test]
     // fn it_solves_the_puzzle() {
     //     let input = include_str!("./input.txt");
-    //     assert_eq!(solve(input), "Hello, world!");
+    //     assert_eq!(solve(input), "7392");
     // }
 }
