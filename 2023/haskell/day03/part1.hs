8321a848bf3cd833122bc5f041005363cc06d140
Louis Smith <15144861+smudger@users.noreply.github.com>
2024-09-21 12:51:21 +0100

    Solve day 3 in Haskell

---
+++ 2023/haskell/day03/part1.hs
@@ -0,0 +1,62 @@
+module Main where
+
+import Data.Char
+import Data.Function
+import Data.List
+
+example :: [String]
+example =
+  [ "467..114..",
+    "...*......",
+    "..35..633.",
+    "......#...",
+    "617*......",
+    ".....+.58.",
+    "..592.....",
+    "......755.",
+    "...$.*....",
+    ".664.598.."
+  ]
+
+gears :: [String] -> [Gear]
+gears = concatMap gearsInRow . zip [0 ..]
+
+gearsInRow :: (Int, String) -> [Gear]
+gearsInRow (y, row) = map (\((x, d) : ys) -> Gear (x, y) (read @Int (d : map snd ys))) . filter (isDigit . snd . head) . groupBy ((==) `on` (isDigit . snd)) . zip [0 ..] $ row
+
+gearLocs :: Gear -> [Location]
+gearLocs Gear {value = v, gear_loc = (x, y)} = map (,y) $ take (length $ show v) [x ..]
+
+markers :: [String] -> [Marker]
+markers = concatMap markersInRow . zip [0 ..]
+
+markersInRow :: (Int, String) -> [Marker]
+markersInRow (y, row) = foldl (\acc (x, c) -> if isDigit c || c == '.' then acc else Marker (x, y) c : acc) [] . zip [0 ..] $ row
+
+markerBox :: Marker -> [Location]
+markerBox Marker {symbol = _, marker_loc = (x, y)} =
+  [ (x - 1, y - 1),
+    (x, y - 1),
+    (x + 1, y - 1),
+    (x - 1, y),
+    (x + 1, y),
+    (x - 1, y + 1),
+    (x, y + 1),
+    (x + 1, y + 1)
+  ]
+
+markedLocs :: [String] -> [Location]
+markedLocs = concatMap markerBox . markers
+
+isValidGear :: [Location] -> Gear -> Bool
+isValidGear locs = any (`elem` locs) . gearLocs
+
+type Location = (Int, Int)
+
+data Gear = Gear {gear_loc :: Location, value :: Int} deriving (Show)
+
+data Marker = Marker {marker_loc :: Location, symbol :: Char} deriving (Show)
+
+main = do
+  input <- readFile "input.txt"
+  print . sum . map value . filter (isValidGear (markedLocs $ lines input)) . gears . lines $ input
