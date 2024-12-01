cfb9f1d4f89db82feac0d272c3b3edb6dbad0423
Louis Smith <15144861+smudger@users.noreply.github.com>
2024-12-01 09:59:15 +0000

    2024 day 1 part 1 in Haskell

---
+++ 2024/haskell/day01/part1.hs
@@ -0,0 +1,19 @@
+module Main where
+
+import Data.FileEmbed (embedStringFile, makeRelativeToProject)
+import Data.List (sort, transpose)
+
+main :: IO ()
+main = do
+  print . solve $ input
+  where
+    input = $(makeRelativeToProject "input.txt" >>= embedStringFile)
+
+solve :: String -> Int
+solve = sum . map (\(a, b) -> abs $ b - a) . zipFromArray . map sort . transpose . map parse . lines
+  where
+    parse = map (read @Int) . words
+    zipFromArray xs = zip (head xs) (head $ drop 1 xs)
+
+example :: String
+example = $(makeRelativeToProject "example.txt" >>= embedStringFile)
