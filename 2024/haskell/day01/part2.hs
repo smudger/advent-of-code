d9214530c1136744b32289a95baa18f7d9a3e479
Louis Smith <15144861+smudger@users.noreply.github.com>
2024-12-01 10:35:24 +0000

    Solve day 1 part 2 in Haskell

--- 2024/haskell/day01/part2.hs
+++ 2024/haskell/day01/part2.hs
@@ -1,12 +1,23 @@
-
 module Main where
 
 import Data.FileEmbed (embedStringFile, makeRelativeToProject)
+import Data.List (group, sort, transpose)
+import Data.Map qualified as M
 
 main :: IO ()
 main = do
   print . solve $ input
-  where input = $(makeRelativeToProject "input.txt" >>= embedStringFile)
+  where
+    input = $(makeRelativeToProject "input.txt" >>= embedStringFile)
 
-solve :: String -> String
-solve x = x
+solve :: String -> Int
+solve x =
+  let parse = map (read @Int) . words
+      counts = map (\l@(x : _) -> (x, length l)) . group . sort
+      (first : xs) = map counts . transpose . map parse . lines $ x
+      (second : _) = xs
+      secondMap = M.fromList second
+   in sum $ map (\(num, count) -> num * count * (M.findWithDefault 0 num secondMap)) first
+
+example :: String
+example = $(makeRelativeToProject "example.txt" >>= embedStringFile)
