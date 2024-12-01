085461058b260586d68c5b685458694e49462759
Louis Smith <15144861+smudger@users.noreply.github.com>
2024-12-01 09:59:15 +0000

    2024 day 1 part 1 in Haskell

---
+++ 2024/haskell/day01/part2.hs
@@ -0,0 +1,12 @@
+
+module Main where
+
+import Data.FileEmbed (embedStringFile, makeRelativeToProject)
+
+main :: IO ()
+main = do
+  print . solve $ input
+  where input = $(makeRelativeToProject "input.txt" >>= embedStringFile)
+
+solve :: String -> String
+solve x = x
---
+++ 2024/haskell/scripts/fetch-input.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+set -u
+set -o pipefail
+
+day=$1
+session=$2
+
+if [ -z "$session" ]; then
+    echo "Session token is not set. Aborting."
+    exit 1
+fi
+
+if [ -z "$day" ]; then
+    echo "Day is not specified. Aborting."
+    exit 1
+fi
+
+day_number=$(echo "$day" | sed 's/^day0*//')
+url="https://adventofcode.com/2024/day/$day_number/input"
+output_dir="$day"
+output_file="$output_dir/input.txt"
+
+mkdir -p "$output_dir"
+
+curl -s --cookie "session=$session" "$url" -o "$output_file"
