489142498daffae36b1edfd24754e8ce50820550
Louis Smith <15144861+smudger@users.noreply.github.com>
2024-12-02 10:38:29 +0000

    Solve day 2 part 2 in Haskell

--- 2024/haskell/day02/part2.hs
+++ 2024/haskell/day02/part2.hs
@@ -1,12 +1,22 @@
-
 module Main where
 
 import Data.FileEmbed (embedStringFile, makeRelativeToProject)
+import Data.List (group, subsequences)
 
 main :: IO ()
 main = do
   print . solve $ input
-  where input = $(makeRelativeToProject "input.txt" >>= embedStringFile)
+  where
+    input = $(makeRelativeToProject "input.txt" >>= embedStringFile)
 
-solve :: String -> String
-solve x = x
+solve :: String -> Int
+solve =
+  let deltas report = map (\(a, b) -> b - a) $ zip report (drop 1 report)
+      checkAllIncreasingOrDecreasing = (== 1) . length . group . map signum . deltas
+      checkLevelDifference = all (\d -> d >= 1 && d <= 3) . map abs . deltas
+      isSafe report = checkAllIncreasingOrDecreasing report && checkLevelDifference report
+      dampen report = filter ((>= (length report) - 1) . length) . subsequences $ report
+   in length . filter (any isSafe) . map (dampen . map (read @Int) . words) . lines
+
+example :: String
+example = $(makeRelativeToProject "example.txt" >>= embedStringFile)
