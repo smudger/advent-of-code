c00897559011aef91329640d255bc391af2761d9
Louis Smith <15144861+smudger@users.noreply.github.com>
2024-12-02 10:26:54 +0000

    Add example to daily template

--- 2024/haskell/daily-template/part1.hs
+++ 2024/haskell/daily-template/part1.hs
@@ -5,7 +5,11 @@ import Data.FileEmbed (embedStringFile, makeRelativeToProject)
 main :: IO ()
 main = do
   print . solve $ input
-  where input = $(makeRelativeToProject "input.txt" >>= embedStringFile)
+  where
+    input = $(makeRelativeToProject "input.txt" >>= embedStringFile)
 
 solve :: String -> String
 solve x = x
+
+example :: String
+example = $(makeRelativeToProject "example.txt" >>= embedStringFile)
--- 2024/haskell/daily-template/part2.hs
+++ 2024/haskell/daily-template/part2.hs
@@ -1,4 +1,3 @@
-
 module Main where
 
 import Data.FileEmbed (embedStringFile, makeRelativeToProject)
@@ -6,7 +5,11 @@ import Data.FileEmbed (embedStringFile, makeRelativeToProject)
 main :: IO ()
 main = do
   print . solve $ input
-  where input = $(makeRelativeToProject "input.txt" >>= embedStringFile)
+  where
+    input = $(makeRelativeToProject "input.txt" >>= embedStringFile)
 
 solve :: String -> String
 solve x = x
+
+example :: String
+example = $(makeRelativeToProject "example.txt" >>= embedStringFile)
