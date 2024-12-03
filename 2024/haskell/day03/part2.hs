dd27f4f3f692e754ba906042abc890ab149a3a63
Louis Smith <15144861+smudger@users.noreply.github.com>
2024-12-03 11:26:06 +0000

    Solve day 3 part 2 in Haskell

--- 2024/haskell/day03/part2.hs
+++ 2024/haskell/day03/part2.hs
@@ -1,6 +1,8 @@
 module Main where
 
+import Data.Char (isDigit)
 import Data.FileEmbed (embedStringFile, makeRelativeToProject)
+import Text.Regex.TDFA
 
 main :: IO ()
 main = do
@@ -8,8 +10,30 @@ main = do
   where
     input = $(makeRelativeToProject "input.txt" >>= embedStringFile)
 
-solve :: String -> String
-solve x = x
+solve :: String -> Int
+solve = snd . foldl' handleInstruction (True, 0) . findValidInstructions
+  where
+    findValidInstructions s = getAllTextMatches (s =~ "mul\\([0-9]{1,3},[0-9]{1,3}\\)|do\\(\\)|don't\\(\\)") :: [String]
+    handleInstruction (_, r) "do()" = (True, r)
+    handleInstruction (_, r) "don't()" = (False, r)
+    handleInstruction (False, r) _ = (False, r)
+    handleInstruction (True, r) i = (True, r + (foldl1 (*) $ nums i))
+
+-- | 'nums' breaks a string up into a list of integers, which were delimited
+-- by non-digit characters (as defined by 'isDigit'), trimming any non-digit characters
+-- at the beginning and at the end. Implementation closely follows implementation
+-- for 'words' in core library.
+--
+-- ==== __Examples__
+--
+-- >>> nums "mul(23,42)"
+-- [23,42]
+nums :: String -> [Int]
+nums s = case dropWhile (not . isDigit) s of
+  "" -> []
+  s' -> (read n) : nums s''
+    where
+      (n, s'') = break (not . isDigit) s'
 
 example :: String
 example = $(makeRelativeToProject "example.txt" >>= embedStringFile)
