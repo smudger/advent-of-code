0fd3d55dd6eb60d2731eebcb08f07bb3d62b1ffe
Louis Smith <15144861+smudger@users.noreply.github.com>
2024-12-04 12:58:15 +0000

    Solve day 4 part 2 in Haskell

--- 2024/haskell/day04/part2.hs
+++ 2024/haskell/day04/part2.hs
@@ -1,6 +1,9 @@
 module Main where
 
 import Data.FileEmbed (embedStringFile, makeRelativeToProject)
+import Data.List
+import Data.Map qualified as M
+import Data.Maybe
 
 main :: IO ()
 main = do
@@ -8,8 +11,27 @@ main = do
   where
     input = $(makeRelativeToProject "input.txt" >>= embedStringFile)
 
-solve :: String -> String
-solve x = x
+solve :: String -> Int
+solve input = length . filter ((< 4) . length . group) . filter ((== 2) . length . filter (== 'M')) . filter ((== 2) . length . filter (== 'S')) . filter ((== 4) . length) . map (filter (\c -> 'M' == c || 'S' == c) . mapMaybe id . tips gridMap) $ centers
+  where
+    centers = M.keys . M.filter (== 'A') $ gridMap
+    gridMap = gridToMap input
 
-example :: String
-example = $(makeRelativeToProject "example.txt" >>= embedStringFile)
+data Direction = North | NorthEast | East | SouthEast | South | SouthWest | West | NorthWest deriving (Show, Enum, Bounded)
+
+type Point = (Int, Int)
+
+tips :: M.Map Point Char -> Point -> [Maybe Char]
+tips m (x, y) = map (\p -> M.lookup p m) [(x - 1, y - 1), (x + 1, y - 1), (x + 1, y + 1), (x - 1, y + 1)]
+
+gridToMap :: String -> M.Map Point Char
+gridToMap = M.fromList . concatMap (\(y, xs) -> map (\(x, c) -> ((x, y), c)) xs) . zip [0 ..] . map (zip [0 ..]) . lines
+
+example1 :: String
+example1 = $(makeRelativeToProject "example1.txt" >>= embedStringFile)
+
+example2 :: String
+example2 = $(makeRelativeToProject "example2.txt" >>= embedStringFile)
+
+example3 :: String
+example3 = $(makeRelativeToProject "example3.txt" >>= embedStringFile)
