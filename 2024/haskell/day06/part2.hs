d3c5a4754c40b56e2f64696bfd8cd73e299ce23a
Louis Smith <15144861+smudger@users.noreply.github.com>
2024-12-06 11:07:04 +0000

    Solve day 6 part 2 slowly

--- 2024/haskell/day06/part2.hs
+++ 2024/haskell/day06/part2.hs
@@ -1,6 +1,8 @@
 module Main where
 
 import Data.FileEmbed (embedStringFile, makeRelativeToProject)
+import Data.List
+import Data.Maybe
 
 main :: IO ()
 main = do
@@ -8,8 +10,47 @@ main = do
   where
     input = $(makeRelativeToProject "input.txt" >>= embedStringFile)
 
-solve :: String -> String
-solve x = x
+solve :: String -> Int
+solve x = length . filter (isLoop x) . drop 1 . nub . mapRoute $ x
+
+mapRoute :: String -> [Point]
+mapRoute x = followPath obstacles dimension (startPoint, North)
+  where
+    obstacles = map fst . filter ((== '#') . snd) . toGrid $ x
+    startPoint = fst . fromJust . find ((== '^') . snd) . toGrid $ x
+    dimension = length . lines $ x
+
+followPath :: [Point] -> Int -> (Point, Direction) -> [Point]
+followPath obstacles dimension v@((x, y), _)
+  | x < 0 || y < 0 || x >= dimension || y >= dimension = []
+  | otherwise = (x, y) : followPath obstacles dimension (hop obstacles v)
+
+isLoop :: String -> Point -> Bool
+isLoop x o = isLoop' (o : obstacles) dimension (startPoint, North) []
+  where
+    obstacles = map fst . filter ((== '#') . snd) . toGrid $ x
+    startPoint = fst . fromJust . find ((== '^') . snd) . toGrid $ x
+    dimension = length . lines $ x
+
+isLoop' :: [Point] -> Int -> (Point, Direction) -> [(Point, Direction)] -> Bool
+isLoop' obstacles dimension v@((x, y), _) seen
+  | x < 0 || y < 0 || x >= dimension || y >= dimension = False
+  | v `elem` seen = True
+  | otherwise = isLoop' obstacles dimension (hop obstacles v) (v : seen)
+
+hop :: [Point] -> (Point, Direction) -> (Point, Direction)
+hop obstacles ((x, y), direction) = case direction of
+  North -> if (x, y - 1) `elem` obstacles then hop obstacles ((x, y), East) else ((x, y - 1), North)
+  East -> if (x + 1, y) `elem` obstacles then hop obstacles ((x, y), South) else ((x + 1, y), East)
+  South -> if (x, y + 1) `elem` obstacles then hop obstacles ((x, y), West) else ((x, y + 1), South)
+  West -> if (x - 1, y) `elem` obstacles then hop obstacles ((x, y), North) else ((x - 1, y), West)
+
+type Point = (Int, Int)
+
+data Direction = North | East | South | West deriving (Show, Eq)
+
+toGrid :: String -> [(Point, Char)]
+toGrid = concatMap (\(y, xs) -> map (\(x, c) -> ((x, y), c)) xs) . zip [0 ..] . map (zip [0 ..]) . lines
 
 example :: String
 example = $(makeRelativeToProject "example.txt" >>= embedStringFile)
