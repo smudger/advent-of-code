292e2b5026b4e9179f626cc11b6d2cf5cac5d102
Louis Smith <15144861+smudger@users.noreply.github.com>
2024-12-06 14:09:28 +0000

    Improve performance by converting from list to set

--- 2024/haskell/day06/part1.hs
+++ 2024/haskell/day06/part1.hs
@@ -3,6 +3,7 @@ module Main where
 import Data.FileEmbed (embedStringFile, makeRelativeToProject)
 import Data.List
 import Data.Maybe
+import Data.Set qualified as S
 
 main :: IO ()
 main = do
@@ -16,21 +17,21 @@ solve = length . nub . mapRoute
 mapRoute :: String -> [Point]
 mapRoute x = followPath obstacles dimension (startPoint, North)
   where
-    obstacles = map fst . filter ((== '#') . snd) . toGrid $ x
+    obstacles = S.fromList . map fst . filter ((== '#') . snd) . toGrid $ x
     startPoint = fst . fromJust . find ((== '^') . snd) . toGrid $ x
     dimension = length . lines $ x
 
-followPath :: [Point] -> Int -> (Point, Direction) -> [Point]
+followPath :: S.Set Point -> Int -> (Point, Direction) -> [Point]
 followPath obstacles dimension v@((x, y), _)
   | x < 0 || y < 0 || x >= dimension || y >= dimension = []
   | otherwise = (x, y) : followPath obstacles dimension (hop obstacles v)
 
-hop :: [Point] -> (Point, Direction) -> (Point, Direction)
+hop :: S.Set Point -> (Point, Direction) -> (Point, Direction)
 hop obstacles ((x, y), direction) = case direction of
-  North -> if (x, y - 1) `elem` obstacles then hop obstacles ((x, y), East) else ((x, y - 1), North)
-  East -> if (x + 1, y) `elem` obstacles then hop obstacles ((x, y), South) else ((x + 1, y), East)
-  South -> if (x, y + 1) `elem` obstacles then hop obstacles ((x, y), West) else ((x, y + 1), South)
-  West -> if (x - 1, y) `elem` obstacles then hop obstacles ((x, y), North) else ((x - 1, y), West)
+  North -> if (x, y - 1) `S.member` obstacles then hop obstacles ((x, y), East) else ((x, y - 1), North)
+  East -> if (x + 1, y) `S.member` obstacles then hop obstacles ((x, y), South) else ((x + 1, y), East)
+  South -> if (x, y + 1) `S.member` obstacles then hop obstacles ((x, y), West) else ((x, y + 1), South)
+  West -> if (x - 1, y) `S.member` obstacles then hop obstacles ((x, y), North) else ((x - 1, y), West)
 
 type Point = (Int, Int)
 
--- 2024/haskell/day06/part2.hs
+++ 2024/haskell/day06/part2.hs
@@ -3,6 +3,7 @@ module Main where
 import Data.FileEmbed (embedStringFile, makeRelativeToProject)
 import Data.List
 import Data.Maybe
+import Data.Set qualified as S
 
 main :: IO ()
 main = do
@@ -11,43 +12,41 @@ main = do
     input = $(makeRelativeToProject "input.txt" >>= embedStringFile)
 
 solve :: String -> Int
-solve x = length . filter (isLoop x) . drop 1 . nub . mapRoute $ x
-
-mapRoute :: String -> [Point]
-mapRoute x = followPath obstacles dimension (startPoint, North)
+solve x = length . map fst . filter (createsLoop startingObstacles dimension) $ potentialObstacles
   where
-    obstacles = map fst . filter ((== '#') . snd) . toGrid $ x
-    startPoint = fst . fromJust . find ((== '^') . snd) . toGrid $ x
+    startingObstacles = S.fromList . map fst . filter ((== '#') . snd) . toGrid $ x
     dimension = length . lines $ x
+    startPoint = fst . fromJust . find ((== '^') . snd) . toGrid $ x
+    potentialObstacles = drop 1 . nubBy (\a b -> fst a == fst b) $ mapRoute startingObstacles dimension (startPoint, North)
 
-followPath :: [Point] -> Int -> (Point, Direction) -> [Point]
-followPath obstacles dimension v@((x, y), _)
+mapRoute :: S.Set Point -> Int -> (Point, Direction) -> [(Point, Direction)]
+mapRoute obstacles dimension v@((x, y), _)
   | x < 0 || y < 0 || x >= dimension || y >= dimension = []
-  | otherwise = (x, y) : followPath obstacles dimension (hop obstacles v)
+  | otherwise = v : mapRoute obstacles dimension (hop obstacles v)
 
-isLoop :: String -> Point -> Bool
-isLoop x o = isLoop' (o : obstacles) dimension (startPoint, North) []
-  where
-    obstacles = map fst . filter ((== '#') . snd) . toGrid $ x
-    startPoint = fst . fromJust . find ((== '^') . snd) . toGrid $ x
-    dimension = length . lines $ x
+createsLoop :: S.Set Point -> Int -> (Point, Direction) -> Bool
+createsLoop obstacles dimension (p@(x, y), d) = case d of
+  North -> isLoop (S.insert p obstacles) dimension ((x, y + 1), North) S.empty
+  East -> isLoop (S.insert p obstacles) dimension ((x - 1, y), East) S.empty
+  South -> isLoop (S.insert p obstacles) dimension ((x, y - 1), South) S.empty
+  West -> isLoop (S.insert p obstacles) dimension ((x + 1, y), West) S.empty
 
-isLoop' :: [Point] -> Int -> (Point, Direction) -> [(Point, Direction)] -> Bool
-isLoop' obstacles dimension v@((x, y), _) seen
+isLoop :: S.Set Point -> Int -> (Point, Direction) -> S.Set (Point, Direction) -> Bool
+isLoop obstacles dimension v@((x, y), _) seen
   | x < 0 || y < 0 || x >= dimension || y >= dimension = False
-  | v `elem` seen = True
-  | otherwise = isLoop' obstacles dimension (hop obstacles v) (v : seen)
+  | v `S.member` seen = True
+  | otherwise = isLoop obstacles dimension (hop obstacles v) (S.insert v seen)
 
-hop :: [Point] -> (Point, Direction) -> (Point, Direction)
+hop :: S.Set Point -> (Point, Direction) -> (Point, Direction)
 hop obstacles ((x, y), direction) = case direction of
-  North -> if (x, y - 1) `elem` obstacles then hop obstacles ((x, y), East) else ((x, y - 1), North)
-  East -> if (x + 1, y) `elem` obstacles then hop obstacles ((x, y), South) else ((x + 1, y), East)
-  South -> if (x, y + 1) `elem` obstacles then hop obstacles ((x, y), West) else ((x, y + 1), South)
-  West -> if (x - 1, y) `elem` obstacles then hop obstacles ((x, y), North) else ((x - 1, y), West)
+  North -> if (x, y - 1) `S.member` obstacles then hop obstacles ((x, y), East) else ((x, y - 1), North)
+  East -> if (x + 1, y) `S.member` obstacles then hop obstacles ((x, y), South) else ((x + 1, y), East)
+  South -> if (x, y + 1) `S.member` obstacles then hop obstacles ((x, y), West) else ((x, y + 1), South)
+  West -> if (x - 1, y) `S.member` obstacles then hop obstacles ((x, y), North) else ((x - 1, y), West)
 
 type Point = (Int, Int)
 
-data Direction = North | East | South | West deriving (Show, Eq)
+data Direction = North | East | South | West deriving (Show, Eq, Ord)
 
 toGrid :: String -> [(Point, Char)]
 toGrid = concatMap (\(y, xs) -> map (\(x, c) -> ((x, y), c)) xs) . zip [0 ..] . map (zip [0 ..]) . lines
