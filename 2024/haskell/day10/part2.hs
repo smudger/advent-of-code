9f5d9a03c18a404ee4e2a3e18c95c379cca072ea
Louis Smith <15144861+smudger@users.noreply.github.com>
2024-12-10 11:55:13 +0000

    Improve part 2 solution

--- 2024/haskell/day10/part2.hs
+++ 2024/haskell/day10/part2.hs
@@ -28,38 +28,40 @@ type TopMap = Map Point Int
 type Trail = [Point]
 
 trailsToSummit :: TopMap -> Point -> [Trail]
-trailsToSummit tmap start = trailsToSummit' tmap (S.singleton [start]) S.empty
+trailsToSummit tmap start = go (S.singleton [start]) S.empty
+  where
+    go inbox outbox
+      | S.null inbox = S.toList outbox
+      | (t, ts) <- (S.elemAt 0 inbox, S.drop 1 inbox) =
+          if isAtSummit tmap t
+            then go ts (S.insert t outbox)
+            else
+              let currHeight = fromJust $ trailHeight tmap t
+                  next = S.filter (isAtHeight tmap (currHeight + 1)) $ trailsFrom t
+               in go (S.union next ts) outbox
 
-trailsToSummit' :: TopMap -> Set Trail -> Set Trail -> [Trail]
-trailsToSummit' tmap inbox outbox
-  | S.null inbox = S.toList outbox
-  | isAtSummit tmap (S.elemAt 0 inbox) = trailsToSummit' tmap (S.drop 1 inbox) (S.insert (S.elemAt 0 inbox) outbox)
-  | otherwise =
-      let t = S.elemAt 0 inbox
-          ts = S.delete t inbox
-          currHeight = fromJust $ height tmap (head t)
-          next = S.filter (atHeight tmap (currHeight + 1)) $ trailsFrom t
-       in trailsToSummit' tmap (S.union next ts) outbox
-
-isAtSummit :: TopMap -> Trail -> Bool
-isAtSummit tmap t = atHeight tmap 9 t
-
--- >>> atHeight (M.fromList [((1, 1), 4)]) 4 (1, 1)
+-- >>> isAtSummit (M.fromList [((1, 1), 9)]) [(1, 1)]
 -- True
--- >>> atHeight (M.fromList [((1, 1), 4)]) 3 (1, 1)
--- False
--- >>> atHeight (M.fromList [((1, 1), 4)]) 3 (0, 0)
--- False
-atHeight :: TopMap -> Int -> Trail -> Bool
-atHeight tmap expected (p : _) = case height tmap p of
+isAtSummit :: TopMap -> Trail -> Bool
+isAtSummit tmap t = isAtHeight tmap 9 t
+
+-- >>> isAtHeight (M.fromList [((1, 1), 4)]) 4 [(1, 1)]
+-- True
+isAtHeight :: TopMap -> Int -> Trail -> Bool
+isAtHeight tmap expected t = case trailHeight tmap t of
   Just h -> h == expected
   Nothing -> False
-atHeight _ _ _ = False
 
--- >>> height (M.fromList [((1, 1), 4)]) (1, 1)
--- 4
-height :: TopMap -> Point -> Maybe Int
-height tmap p = M.lookup p tmap
+-- >>> trailHeight (M.fromList [((1, 1), 4)]) [(1, 1)]
+-- Just 4
+trailHeight :: TopMap -> Trail -> Maybe Int
+trailHeight tmap (p : _) = pointHeight tmap p
+trailHeight _ _ = Nothing
+
+-- >>> pointHeight (M.fromList [((1, 1), 4)]) (1, 1)
+-- Just 4
+pointHeight :: TopMap -> Point -> Maybe Int
+pointHeight tmap p = M.lookup p tmap
 
 -- >>> inMap (M.fromList [(1, 3)]) 2
 -- False
@@ -70,8 +72,9 @@ inMap xs x = M.member x xs
 
 -- >>> trailsFrom [(2, 1)]
 -- fromList [[(1,1),(2,1)],[(2,0),(2,1)],[(2,2),(2,1)],[(3,1),(2,1)]]
-trailsFrom :: [Point] -> Set [Point]
+trailsFrom :: Trail -> Set Trail
 trailsFrom t@(x : _) = S.map (\d -> ((x .+. d) : t)) directions
+trailsFrom _ = S.empty
 
 directions :: Set Point
 directions = S.fromList [(1, 0), (-1, 0), (0, 1), (0, -1)]
