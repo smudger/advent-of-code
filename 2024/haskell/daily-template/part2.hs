969120fa132dd37b9b75d128108401802087d392
Louis Smith <15144861+smudger@users.noreply.github.com>
2024-12-14 12:08:31 +0000

    Update daily template

--- 2024/haskell/daily-template/part2.hs
+++ 2024/haskell/daily-template/part2.hs
@@ -3,13 +3,13 @@ module Main where
 import Data.FileEmbed (embedStringFile, makeRelativeToProject)
 
 main :: IO ()
-main = do
-  print . solve $ input
-  where
-    input = $(makeRelativeToProject "input.txt" >>= embedStringFile)
+main = print . solve $ input
 
 solve :: String -> String
 solve x = x
 
 example :: String
 example = $(makeRelativeToProject "example.txt" >>= embedStringFile)
+
+input :: String
+input = $(makeRelativeToProject "input.txt" >>= embedStringFile)
