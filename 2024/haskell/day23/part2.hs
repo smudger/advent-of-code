e2913f322ebbfee96a1145cda722dafc14ab8e92
Louis Smith <15144861+smudger@users.noreply.github.com>
2024-12-23 17:44:09 +0000

    Remove unused functions

--- 2024/haskell/day23/part2.hs
+++ 2024/haskell/day23/part2.hs
@@ -44,22 +44,10 @@ isNeighbour (x : xs) a b =
     then True
     else isNeighbour xs a b
 
--- >>> startsWithT ["ab", "cd", "te"]
--- True
--- >>> startsWithT ["ab", "cd", "et"]
--- False
-startsWithT :: [Computer] -> Bool
-startsWithT = any ((== 't') . head)
-
 -- >>> computers "ab-cd\nef-ab"
 -- ["ab","cd","ef"]
 computers :: Input -> [Computer]
 computers = nub . chunksOf 2 . filter isAsciiLower
-
--- >>> setsOfThree ["ab", "cd", "ef", "gh"]
--- [["ab","cd","ef"],["ab","cd","gh"],["ab","ef","gh"],["cd","ef","gh"]]
-setsOfThree :: [Computer] -> [[Computer]]
-setsOfThree xs = [a : b : c : [] | (a : as) <- tails xs, (b : bs) <- tails as, (c : _) <- tails bs]
 
 -- >>> network "ab-cd\nef-ab"
 -- [("ab","cd"),("ef","ab")]
@@ -68,22 +56,6 @@ network = map (pair . chunksOf 2) . chunksOf 4 . filter isAsciiLower
   where
     pair (a : b : _) = (a, b)
     pair _ = error "Invalid input"
-
--- >>> isTriangle [("ab", "cd"), ("ab", "ef"), ("ef", "cd")] ["ab", "cd", "ef"]
--- True
--- >>> isTriangle [("ab", "cd"), ("ab", "ef"), ("ef", "gh")] ["ab", "cd", "ef"]
--- False
-isTriangle :: [Connection] -> [Computer] -> Bool
-isTriangle n (a : b : c : _) =
-  (== 3) . length . filter (\x -> x `elem` n) $
-    [ (a, b),
-      (b, a),
-      (a, c),
-      (c, a),
-      (b, c),
-      (c, b)
-    ]
-isTriangle _ _ = error "Wrong number of vertices"
 
 -- >>> chunksOf 2 "abcdef"
 -- ["ab","cd","ef"]
