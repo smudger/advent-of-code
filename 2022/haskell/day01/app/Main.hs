4fe1f58c8c34ecd96eee10f550011b295b04e84c
Louis Smith <15144861+smudger@users.noreply.github.com>
2025-10-23 23:19:54 +0100

    Improve solution

--- 2022/haskell/day01/app/Main.hs
+++ 2022/haskell/day01/app/Main.hs
@@ -11,29 +11,22 @@ main = do
   print (part2 input)
 
 part1 :: String -> Int
-part1 = maximum . map sumCalories . groupByElf . lines
+part1 = maximum . map sumCalories . elves . lines
 
 part2 :: String -> Int
-part2 = sum . take 3 . reverse . sort . map sumCalories . groupByElf . lines
+part2 = sum . take 3 . reverse . sort . map sumCalories . elves . lines
 
 -- >>> sumCalories ["1", "2", "3"]
 -- 6
 sumCalories :: [String] -> Int
 sumCalories = foldl' (\acc x -> acc + read x) 0
 
--- >>> groupByElf ["1", "2", "", "3", "4", "5"]
--- [["1","2"],["3","4","5"]]
-groupByElf :: [String] -> [[String]]
-groupByElf xs = case dropWhile (== "") xs of
+-- See source code for `words`
+-- >>> elves ["", "", "1", "2", "", "", "", "3", "4", "5", "", "6", ""]
+-- [["1","2"],["3","4","5"],["6"]]
+elves :: [String] -> [[String]]
+elves xs = case dropWhile (== "") xs of
   [] -> []
-  xs' -> e : groupByElf xs''
+  xs' -> x : elves xs''
     where
-      (e, xs'') = break (== "") xs'
-
--- >>> prependToFirst 1 []
--- [[1]]
--- >>> prependToFirst 1 [[2, 2], [3, 3, 3]]
--- [[1,2,2],[3,3,3]]
-prependToFirst :: a -> [[a]] -> [[a]]
-prependToFirst x [] = [[x]]
-prependToFirst x (y : ys) = (x : y) : ys
+      (x, xs'') = break (== "") xs'
