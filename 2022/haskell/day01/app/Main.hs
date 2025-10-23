d408d4bc33bb564956921e91deb6410bb2485776
Louis Smith <15144861+smudger@users.noreply.github.com>
2025-10-23 22:09:17 +0100

    Solve day01 part 1 and 2 inefficiently

--- 2022/haskell/day01/app/Main.hs
+++ 2022/haskell/day01/app/Main.hs
@@ -1,5 +1,7 @@
 module Main (main) where
 
+import Data.List (sort)
+
 main :: IO ()
 main = do
   input <- getContents
@@ -7,7 +9,30 @@ main = do
   print (part2 input)
 
 part1 :: String -> Int
-part1 input = length (lines input)
+part1 = maximum . map sumCalories . groupByElf . lines
 
 part2 :: String -> Int
-part2 _input = 0
+part2 = sum . take 3 . reverse . sort . map sumCalories . groupByElf . lines
+
+-- >>> sumCalories ["1", "2", "3"]
+-- 6
+sumCalories :: [String] -> Int
+sumCalories [] = 0
+sumCalories (x : xs) = read x + sumCalories xs
+
+-- >>> groupByElf ["1", "2", "", "3", "4", "5"]
+-- [["1","2"],["3","4","5"]]
+groupByElf :: [String] -> [[String]]
+groupByElf [] = []
+groupByElf (x : xs) =
+  if x == ""
+    then [] : groupByElf xs
+    else prependToFirst x (groupByElf xs)
+
+-- >>> prependToFirst 1 []
+-- [[1]]
+-- >>> prependToFirst 1 [[2, 2], [3, 3, 3]]
+-- [[1,2,2],[3,3,3]]
+prependToFirst :: a -> [[a]] -> [[a]]
+prependToFirst x [] = [[x]]
+prependToFirst x (y : ys) = (x : y) : ys
