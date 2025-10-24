b4ad6619397f9e28cf1bc86bc020645d1e330d2e
Louis Smith <15144861+smudger@users.noreply.github.com>
2025-10-24 14:35:07 +0100

    Update daily template to include tests

--- 2022/haskell/daily-template/app/Main.hs
+++ 2022/haskell/daily-template/app/Main.hs
@@ -1,13 +1,9 @@
 module Main (main) where
 
+import Solver (part1, part2)
+
 main :: IO ()
 main = do
   input <- getContents
   print (part1 input)
   print (part2 input)
-
-part1 :: String -> Int
-part1 _input = 0
-
-part2 :: String -> Int
-part2 _input = 0
