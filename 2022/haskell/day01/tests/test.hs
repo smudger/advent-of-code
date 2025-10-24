3160627781a125ea941b353d291fbe05862d6979
Louis Smith <15144861+smudger@users.noreply.github.com>
2025-10-24 16:16:28 +0100

    Add model-based property tests

--- 2022/haskell/day01/tests/test.hs
+++ 2022/haskell/day01/tests/test.hs
@@ -1,5 +1,6 @@
 module Main (main) where
 
+import Data.List (sort)
 import Solver (part1, part2)
 import Test.Tasty
 import Test.Tasty.HUnit
@@ -26,7 +27,8 @@ propertyTests =
     [ {--
         Invariants
       --}
-      testProperty "All generated inputs are valid" prop_ValidInput,
+      testProperty "All generated string inputs are valid" prop_ValidStringInput,
+      testProperty "All generated model inputs are valid" prop_ValidModelInput,
       {--
         Postconditions
       --}
@@ -38,7 +40,7 @@ propertyTests =
       testProperty "Part 1 solution increases or stays the same if you add another digit" (prop_AddDigit part1),
       testProperty "Part 2 solution increases or stays the same if you add another digit" (prop_AddDigit part2),
       testProperty "Part 1 solution decreases or stays the same if you add another newline" (prop_AddNewline part1),
-      testProperty "Part 2 solution decreases or stays the same if you add another newline" (prop_AddNewline part2)
+      testProperty "Part 2 solution decreases or stays the same if you add another newline" (prop_AddNewline part2),
       {--
         Inductive properties
             Not possible in this case: We cannot easily define `part1 ('1' : "2\n3")`, given the solution to `part1 "2\n3"`.
@@ -46,8 +48,11 @@ propertyTests =
       --}
       {--
         Model-based properties
-            Not possible in this case: There is no apparent model we could abstract to without reimplementing the function under test.
+            These aren't great examples of model-based properties as they partially re-implement the function under test.
+            However, they do give us some security that our string parsing is correct.
       --}
+      testProperty "Part 1 solution gives the same answer as our model" prop_ModelPart1,
+      testProperty "Part 2 solution gives the same answer as our model" prop_ModelPart2
     ]
 
 validChars :: [Char]
@@ -59,9 +64,24 @@ newtype Input
 
 instance Arbitrary Input where
   arbitrary = Input <$> listOf (elements validChars)
+  shrink (Input i) = Input <$> shrink i
 
-prop_ValidInput :: Input -> Bool
-prop_ValidInput (Input s) = all (`elem` validChars) s
+newtype Model
+  = Model [[Integer]]
+  deriving (Show)
+
+instance Arbitrary Model where
+  arbitrary = Model <$> listOf (listOf (elements [0 .. 10000]))
+  shrink (Model m) = Model <$> shrink m
+
+valid :: String -> Bool
+valid = all (`elem` validChars)
+
+prop_ValidStringInput :: Input -> Bool
+prop_ValidStringInput (Input s) = valid s
+
+prop_ValidModelInput :: Model -> Bool
+prop_ValidModelInput (Model m) = valid (toString m)
 
 prop_TotalSum :: (String -> Integer) -> Input -> Bool
 prop_TotalSum f (Input s) = f s <= totalSum s
@@ -93,6 +113,21 @@ insertAt :: Int -> a -> [a] -> [a]
 insertAt _ x [] = [x]
 insertAt 0 x ys = x : ys
 insertAt n x (y : ys) = y : insertAt (n - 1) x ys
+
+prop_ModelPart1 :: Model -> Bool
+prop_ModelPart1 (Model m) = part1 (toString m) == modelResult m
+  where
+    modelResult xs = case map sum xs of
+      [] -> 0
+      xs' -> maximum xs'
+
+prop_ModelPart2 :: Model -> Bool
+prop_ModelPart2 (Model m) = part2 (toString m) == modelResult m
+  where
+    modelResult = sum . take 3 . reverse . sort . map sum
+
+toString :: [[Integer]] -> String
+toString = unlines . map (unlines . map show)
 
 {----------------------------------------------------------------------------------------------------------------------
     Unit Tests
