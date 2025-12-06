6cc71a2350e4b3866f2a8e83cbc5a3ad1021a8cc
Louis Smith <15144861+smudger@users.noreply.github.com>
2025-12-06 21:32:46 +0000

    Solve day06 part 2

--- 2025/haskell/day06/app/Solver.hs
+++ 2025/haskell/day06/app/Solver.hs
@@ -8,6 +8,9 @@ import Data.List (foldl1', transpose)
 
 solveAll :: [Equation] -> Int
 solveAll = sum . map solve
+  where
+    solve :: Equation -> Int
+    solve = uncurry foldl1'
 
 parse1 :: String -> [Equation]
 parse1 = map equation . transpose . map words . lines
@@ -21,17 +24,7 @@ parse1 = map equation . transpose . map words . lines
             _ -> undefined
 
 parse2 :: String -> [Equation]
-parse2 = map equation . transpose . map words . lines
-  where
-    equation :: [String] -> Equation
-    equation xs =
-      let nums = parseNums (init xs)
-       in case last xs of
-            "*" -> ((*), nums)
-            "+" -> ((+), nums)
-            _ -> undefined
-    parseNums :: [String] -> [Int]
-    parseNums = map read
+parse2 input = map parseEq (equations input)
 
 {----------------------------------------------------------------------------------------------------------------------
     Helpers
@@ -41,5 +34,27 @@ type Operation = Int -> Int -> Int
 
 type Equation = (Operation, [Int])
 
-solve :: Equation -> Int
-solve = uncurry foldl1'
+equations :: String -> [[String]]
+equations eqs =
+  let opsLine = last (lines eqs)
+      starts :: [Int]
+      starts = [i | (c, i) <- zip opsLine [0 ..], c /= ' ']
+      ranges :: [(Int, Int)]
+      ranges = zip starts (drop 1 starts ++ [length opsLine + 1])
+   in map extractEq ranges
+  where
+    extractEq :: (Int, Int) -> [String]
+    extractEq (s, e) = map (take (e - (s + 1)) . drop s) . lines $ eqs
+
+parseEq :: [String] -> Equation
+parseEq s =
+  let op = case last s of
+        (o : _) -> parseOp o
+        _ -> undefined
+      nums = map read . filter (not . all (== ' ')) . transpose . init $ s
+   in (op, nums)
+
+parseOp :: Char -> Operation
+parseOp '*' = (*)
+parseOp '+' = (+)
+parseOp _ = undefined
--- 2025/haskell/day06/tests/test.hs
+++ 2025/haskell/day06/tests/test.hs
@@ -1,7 +1,7 @@
 module Main (main) where
 
 import Data.List (sort)
-import Solver (part1, part2)
+import Solver (parse1, parse2, solveAll)
 import Test.Tasty
 import Test.Tasty.HUnit
 import Test.Tasty.QuickCheck
@@ -58,14 +58,14 @@ unitTests =
     ]
 
 assert_Part1Example1 :: Assertion
-assert_Part1Example1 = part1 example1 @?= 0
+assert_Part1Example1 = solveAll (parse1 example1) @?= 4277556
 
 assert_Part2Example1 :: Assertion
-assert_Part2Example1 = part2 example1 @?= 0
+assert_Part2Example1 = solveAll (parse2 example1) @?= 3263827
 
 {----------------------------------------------------------------------------------------------------------------------
     Examples
 ----------------------------------------------------------------------------------------------------------------------}
 
 example1 :: String
-example1 = ""
+example1 = "123 328  51 64 \n 45 64  387 23 \n  6 98  215 314\n*   +   *   +  "
