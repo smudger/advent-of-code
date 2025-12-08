34222fc5c57ed5c4f6811044bee9a0bc31e82505
Louis Smith <15144861+smudger@users.noreply.github.com>
2025-12-08 16:25:05 +0000

    Solve day 8 quickly

--- 2025/haskell/day08/app/Solver.hs
+++ 2025/haskell/day08/app/Solver.hs
@@ -2,8 +2,8 @@ module Solver where
 
 import Data.Char (isDigit)
 import Data.Function (on)
-import Data.List (find, partition, sortBy, tails, union)
-import Data.Maybe (fromJust)
+import Data.List (partition, sortBy, tails)
+import Data.Maybe (listToMaybe)
 import Data.Set (Set)
 import Data.Set qualified as S
 
@@ -16,16 +16,14 @@ part1 xs =
   let distances :: [((Point, Point), Double)]
       distances = take (if length xs < 1000 then 10 else 1000) . sortBy (compare `on` snd) . concatMap calcDistances . tails $ xs
       circuits :: [[Point]]
-      circuits = connect $ map fst distances
+      circuits = map S.toList . connect . map fst $ distances
    in product . map length . take 3 . reverse . sortBy (compare `on` length) $ circuits
 
 part2 :: [Point] -> Int
 part2 xs =
-  let distances :: [(Point, Point)]
-      distances = map fst . sortBy (compare `on` snd) . concatMap calcDistances . tails $ xs
-      xs' = S.fromList xs
-      n' = fromJust $ find (\n -> head (connectN n distances) == xs') [(length xs) .. (length xs) ^ (2 :: Integer)]
-      ((x1, _, _), (x2, _, _)) = distances !! (n' - 1)
+  let pairs = map fst . sortBy (compare `on` snd) . concatMap calcDistances . tails $ xs
+      n = firstTotalConnection (S.fromList xs) pairs
+      ((x1, _, _), (x2, _, _)) = pairs !! n
    in x1 * x2
 
 parse :: String -> [Point]
@@ -60,30 +58,22 @@ distance (x1, y1, z1) (x2, y2, z2) =
       z = fromIntegral $ (z2 - z1) ^ (2 :: Integer)
    in sqrt $ x + y + z
 
-connect :: [(Point, Point)] -> [[Point]]
-connect [] = []
-connect ((p1, p2) : ps) =
-  let cs = connect ps
-      (matches, cs') = partition (\c -> p1 `elem` c || p2 `elem` c) cs
+connect :: [(Point, Point)] -> [Set Point]
+connect = foldl' update []
+
+update :: [Set Point] -> (Point, Point) -> [Set Point]
+update cs (p1, p2) =
+  let (matches, cs') = partition (\c -> S.member p1 c || S.member p2 c) cs
       new = case matches of
-        [] -> [p1, p2]
-        [c] -> union c [p1, p2]
-        [c1, c2] -> union c1 c2
+        [] -> S.fromList [p1, p2]
+        [c] -> S.union c (S.fromList [p1, p2])
+        [c1, c2] -> S.union c1 c2
         _ -> error "too many matches"
    in new : cs'
 
-connect' :: [(Point, Point)] -> [Set Point]
-connect' = foldl' update []
+firstTotalConnection :: Set Point -> [(Point, Point)] -> Int
+firstTotalConnection points pairs = connectAux (connect $ take (S.size points) pairs) (S.size points)
   where
-    update :: [Set Point] -> (Point, Point) -> [Set Point]
-    update cs (p1, p2) =
-      let (matches, cs') = partition (\c -> S.member p1 c || S.member p2 c) cs
-          new = case matches of
-            [] -> S.fromList [p1, p2]
-            [c] -> S.union c (S.fromList [p1, p2])
-            [c1, c2] -> S.union c1 c2
-            _ -> error "too many matches"
-       in new : cs'
-
-connectN :: Int -> [(Point, Point)] -> [Set Point]
-connectN n = connect' . take n
+    connectAux circuits n
+      | listToMaybe circuits == Just points = n
+      | otherwise = connectAux (update circuits (pairs !! (n + 1))) (n + 1)
